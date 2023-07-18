6cedf9caa0224b5e3a0b4c4776c7aa893e488ecd
Crowdbotics <engineering@crowdbotics.com>
2023-07-18 12:59:43 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "proud-recipe-dev-102594",
+  url: "https://proud-recipe-dev-102594.botics.co",
+  api: "https://proud-recipe-dev-102594.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
